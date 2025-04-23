58ae0578351e6dddf0b34af1641c124fb3ced089
dont-wait <nguyentansang15042005@gmail.com>
2025-04-23 18:39:38 +0700

    "add method in CategoryMapper"

--- server/src/main/java/com/nhom678/server/mapper/CategoryMapper.java
+++ server/src/main/java/com/nhom678/server/mapper/CategoryMapper.java
@@ -1,8 +1,15 @@
 package com.nhom678.server.mapper;
 
+import com.nhom678.server.dto.request.category.CreateCategoryRequest;
+import com.nhom678.server.dto.request.category.UpdateCategoryRequest;
+import com.nhom678.server.dto.response.CategoryResponse;
+import com.nhom678.server.entity.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
-
+    CategoryResponse toCategoryResponse(CategoryResponse request);
+    Category toCategory(CreateCategoryRequest request);
+    void updateCategory(@MappingTarget Category category, UpdateCategoryRequest request);
 }
