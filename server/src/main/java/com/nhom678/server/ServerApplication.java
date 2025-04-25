696f9b5e3858f7c719a95560d535c0f77e9762a2
dont-wait <nguyentansang15042005@gmail.com>
2025-04-26 02:47:22 +0700

    feat: implement .env

--- server/src/main/java/com/nhom678/server/ServerApplication.java
+++ server/src/main/java/com/nhom678/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.nhom678.server;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
 	public static void main(String[] args) {
+		//Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure().directory("/home/dontwait/programming/_huit-library/server").load();
+		System.out.println(dotenv.get("DB_URL"));
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
