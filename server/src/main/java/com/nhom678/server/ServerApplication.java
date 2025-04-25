e0a04cf526f0ede7113d203adfd31aedab5d47f9
dont-wait <nguyentansang15042005@gmail.com>
2025-04-26 03:04:44 +0700

    fix: debug by handmade error

--- server/src/main/java/com/nhom678/server/ServerApplication.java
+++ server/src/main/java/com/nhom678/server/ServerApplication.java
@@ -1,5 +1,6 @@
 package com.nhom678.server;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().directory("/home/dontwait/programming/_huit-library/server").load();
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
--- server/src/main/java/com/nhom678/server/mapper/CategoryMapper.java
+++ server/src/main/java/com/nhom678/server/mapper/CategoryMapper.java
@@ -11,6 +11,5 @@ import org.mapstruct.MappingTarget;
 public interface CategoryMapper {
     CategoryResponse toCategoryResponse(Category category);
     Category toCategory(CreateCategoryRequest request);
-    CategoryResponse
-    updateCategory(@MappingTarget Category category, UpdateCategoryRequest request);
+    void updateCategory(@MappingTarget Category category, UpdateCategoryRequest request);
 }
