b4f93dca7034b9ed36d68d4180e4c3fd58189bc1
dont-wait <nguyentansang15042005@gmail.com>
2025-05-03 19:55:07 +0700

    fix: remove line cause call publisher have many book... recursion

--- server/src/main/java/com/nhom678/server/dto/response/CategoryResponse.java
+++ server/src/main/java/com/nhom678/server/dto/response/CategoryResponse.java
@@ -1,16 +1,13 @@
 package com.nhom678.server.dto.response;
 
-import com.nhom678.server.entity.Book;
 import lombok.Data;
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Data
 @Getter
 public class CategoryResponse {
     int categoryId;
     String categoryName;
-    List<Book> bookList = new ArrayList<>();
 }
--- server/src/main/java/com/nhom678/server/dto/response/PublisherResponse.java
+++ server/src/main/java/com/nhom678/server/dto/response/PublisherResponse.java
@@ -1,13 +1,10 @@
 package com.nhom678.server.dto.response;
 
-import com.nhom678.server.entity.Book;
 import lombok.Data;
 
-import java.util.List;
 
 @Data
 public class PublisherResponse {
     Integer publisherId;
     String publisherName;
-    List<Book> books; //when call this response we will see list book the publisher have
 }
