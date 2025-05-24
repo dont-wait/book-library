9420c6a855cc305ff661247747ac5679e69eab14
dont-wait <nguyentansang15042005@gmail.com>
2025-05-24 13:16:35 +0700

    feat(security): configure JWT-based security architecture

---
+++ server/src/main/java/com/nhom678/server/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.nhom678.server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //Chua nhung public endpoint ai cx truy cap duoc
+    private final String[] PUBLIC_URLS = {
+            "/authen/**",
+            "/api-ui.html",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-resources/**",
+            "/webjars/**",
+    };
+    @Value("${JWT_SECRET}")
+    private String signerKey;
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable) //ngan ngua cross-site -> VIP but in this case, i still use that, so chill
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(PUBLIC_URLS).permitAll() //authen
+//                        .requestMatchers(HttpMethod.POST, "/librarians", "/admin").hasRole("ADMIN") //Chi ai co quyen cao hon moi dc post
+//                        .requestMatchers(HttpMethod.POST, "/members").hasAnyRole("LIBRARIAN", "ADMIN")
+                .anyRequest().authenticated());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() { //oauth2 resource include nimbus jwt decoder
+        SecretKeySpec secretKeySpec = new SecretKeySpec(signerKey.getBytes(), "HS512");
+
+        return NimbusJwtDecoder
+                .withSecretKey(secretKeySpec)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+    }
+}
